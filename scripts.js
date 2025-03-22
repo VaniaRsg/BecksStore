b95c726c2d1604ef44299cddbf7c958839894697
Vania Rsg <196737495+VaniaRsg@users.noreply.github.com>
2025-03-21 21:44:24 -0300

    vercel correção

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// Adicione este trecho para inicialização
-document.addEventListener('DOMContentLoaded', () => {
-    if (window.speedInsights) {
-      window.speedInsights.init({
-        dsn: 'SUA_CHAVE_DNS_AQUI', // Obtenha em https://vercel.com/docs/speed-insights
-        framework: 'vue'
-      })
-    }
-  })
